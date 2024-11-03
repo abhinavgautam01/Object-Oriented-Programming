7205854070db51af751c15d08d03852f586eefc1
abhinavgautam01 <abgautam1017@gmail.com>
2024-11-03 21:11:44 +0530

    c++

---
+++ sorting_practice.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void display(int arr[], int n){
+    int i;
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<"\t";
+    }
+}
+void inputArray(int arr[], int n){
+    int i;
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+void swap(int *a, int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int main(){
+    int n=5,i,a=1,b=3;
+    int arr[n];
+    inputArray(arr,n);
+    display(arr,n);
+    cout<<"\nValues before swapping\na : "<<a<<" and b : "<<b<<endl;
+    swap(&a,&b);
+    cout<<"\nValues after swapping\na : "<<a<<" and b : "<<b<<endl;
+}
