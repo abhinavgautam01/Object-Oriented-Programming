9b82481daa6dce837365ba9630c651d65f316f50
abhinavgautam01 <abgautam1017@gmail.com>
2024-12-11 22:59:31 +0530

    deleted

--- practice4.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstring>
-#include <iostream>
-using namespace std;
-
-int main() {
-    const char* str = "Hello, World!";
-    cout << "Length: " << strlen(str) << endl; // Output: Length: 13
-    return 0;
-}
